3bd62eca2d4a66653196a8c58bdffac79dfa4490
aky <akiyugata271@gmail.com>
2025-12-11 20:22:49 +0900

    add pin define

--- main/main.c
+++ main/main.c
@@ -2,8 +2,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "driver/i2c_master.h"      // ESP-IDF v5 の新I2Cドライバ
+#include "driver/i2c_master.h"
 #include "vl53l5cx_api.h"
+
+#define I2C_PORT           I2C_NUM_1
+#define I2C_SDA_GPIO       1        // 自分の配線に合わせて変更
+#define I2C_SCL_GPIO       2        // 自分の配線に合わせて変更
 
 void app_main(void)
 {
