41f566bf2770b396e225409ab5237265f5d9d190
aky <akiyugata271@gmail.com>
2025-12-11 20:22:15 +0900

    add include

--- main/main.c
+++ main/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/i2c_master.h"      // ESP-IDF v5 の新I2Cドライバ
+#include "vl53l5cx_api.h"
 
 void app_main(void)
 {
