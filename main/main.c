7e22d92dc04e886d7a7bfe10827ca499fb787e23
aky <akiyugata271@gmail.com>
2025-12-12 15:10:52 +0900

    update

--- main/main.c
+++ main/main.c
@@ -79,13 +79,24 @@ void app_main(void)
         }
     }
     bsp_display_unlock();
+    ESP_ERROR_CHECK(vl53l5cx_settings_init());
 
     while (true)
     {
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        lv_obj_set_style_bg_color(cells[3][3], lv_palette_main(LV_PALETTE_RED), 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        lv_obj_set_style_bg_color(cells[3][3], lv_palette_main(LV_PALETTE_BLUE), 0);
+        status = vl53l5cx_check_data_ready(&Dev, &isReady);if(isReady)
+        {
+            vl53l5cx_get_ranging_data(&Dev, &Results);
+
+            for(int i = 0; i < 16; i++)
+            {
+                int alpha = Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i] / 2000 * 255;
+                lv_obj_set_style_bg_opa(cells[(int)(i / 4)][(int)(i % 4)], alpha, 0);
+            }
+        }
+
+        /* Wait a few ms to avoid too high polling (function in platform
+         * file, not in API) */
+        WaitMs(&(Dev.platform), 5);
     }
 }
 
@@ -128,41 +139,6 @@ esp_err_t my_backlight_set(int percent)
     ESP_ERROR_CHECK(ledc_set_duty(LCD_LEDC_MODE, LCD_LEDC_CHANNEL, duty));
     ESP_ERROR_CHECK(ledc_update_duty(LCD_LEDC_MODE, LCD_LEDC_CHANNEL));
     return ESP_OK;
-    ESP_ERROR_CHECK(vl53l5cx_settings_init());
-
-    while(true)
-    {
-        /* Use polling function to know when a new measurement is ready.
-         * Another way can be to wait for HW interrupt raised on PIN A1
-         * (INT) when a new measurement is ready */
-
-        status = vl53l5cx_check_data_ready(&Dev, &isReady);
-
-        if(isReady)
-        {
-            vl53l5cx_get_ranging_data(&Dev, &Results);
-
-            /* As the sensor is set in 4x4 mode by default, we have a total
-             * of 16 zones to print. For this example, only the data of first zone are
-             * print */
-            printf("Print data no : %3u\n", Dev.streamcount);
-            for(int i = 0; i < 16; i++)
-            {
-                printf("Zone : %3d, Status : %3u, Distance : %4d mm\n",
-                       i,
-                       Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i],
-                       Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i]);
-            }
-            printf("\n");
-        }
-
-        /* Wait a few ms to avoid too high polling (function in platform
-         * file, not in API) */
-        WaitMs(&(Dev.platform), 5);
-    }
-
-    status = vl53l5cx_stop_ranging(&Dev);
-    printf("End of ULD demo\n");
 }
 
 esp_err_t vl53l5cx_settings_init(void)
