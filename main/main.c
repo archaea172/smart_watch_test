497bc2edbb862acd1eb90f24d73ae35b64845ed9
aky <akiyugata271@gmail.com>
2025-12-12 09:41:24 +0900

    add include

--- main/main.c
+++ main/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "bsp/esp-bsp.h"
+#include "lvgl.h"
+
+#include "driver/ledc.h"
+
 void app_main(void)
 {
 
