be6cadf97806dd1115fc11a78d257b1b2ca73a3e
aky <akiyugata271@gmail.com>
2025-12-12 09:50:59 +0900

    remove motor pwm

--- main/main.c
+++ main/main.c
@@ -27,14 +27,15 @@ static const char *TAG = "app_main";
 
 esp_err_t my_backlight_init(void);
 esp_err_t my_backlight_set(int percent);
-void pwm_init(void);
-void pwm_set_percent(int percent);
 
 static lv_display_t *disp = NULL;
 
 void app_main(void)
 {
-
+    disp = bsp_display_start();
+    ESP_ERROR_CHECK(my_backlight_init());
+    ESP_ERROR_CHECK(my_backlight_set(20));  
+    ESP_LOGI(TAG, "Display initialized");
 }
 
 esp_err_t my_backlight_init(void)
@@ -76,41 +77,4 @@ esp_err_t my_backlight_set(int percent)
     ESP_ERROR_CHECK(ledc_set_duty(LCD_LEDC_MODE, LCD_LEDC_CHANNEL, duty));
     ESP_ERROR_CHECK(ledc_update_duty(LCD_LEDC_MODE, LCD_LEDC_CHANNEL));
     return ESP_OK;
-}
-
-void pwm_init(void)
-{
-    ledc_timer_config_t timer = {
-        .speed_mode = PWM_MODE,
-        .duty_resolution  = PWM_DUTY_RES,
-        .timer_num        = PWM_TIMER,
-        .freq_hz          = PWM_FREQ_HZ,
-        .clk_cfg          = LEDC_AUTO_CLK,
-    };
-    ESP_ERROR_CHECK(ledc_timer_config(&timer));
-
-    // 2) チャンネル設定（どのピンから出すか）
-    ledc_channel_config_t channel = {
-        .gpio_num       = MOTOR_PIN,
-        .speed_mode     = PWM_MODE,
-        .channel        = PWM_CHANNEL,
-        .timer_sel      = PWM_TIMER,
-        .duty           = 0,                 // 初期 duty = 0%
-        .hpoint         = 0,
-        .intr_type      = LEDC_INTR_DISABLE,
-        .flags.output_invert = 0,           // 反転したければ 1
-    };
-    ESP_ERROR_CHECK(ledc_channel_config(&channel));
-}
-
-void pwm_set_percent(int percent)
-{
-    if (percent < 0)   percent = 0;
-    if (percent > 100) percent = 100;
-
-    uint32_t duty_max = (1 << PWM_DUTY_RES) - 1;      // 2^10 - 1 = 1023
-    uint32_t duty = duty_max * percent / 100;
-
-    ESP_ERROR_CHECK(ledc_set_duty(PWM_MODE, PWM_CHANNEL, duty));
-    ESP_ERROR_CHECK(ledc_update_duty(PWM_MODE, PWM_CHANNEL));
 }
