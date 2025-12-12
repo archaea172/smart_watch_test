183353a0a82b0844f040701d286f435788a0bc68
aky <akiyugata271@gmail.com>
2025-12-12 09:41:36 +0900

    add macro

--- main/main.c
+++ main/main.c
@@ -9,6 +9,20 @@
 
 #include "driver/ledc.h"
 
+#define LCD_LEDC_TIMER      LEDC_TIMER_0
+#define LCD_LEDC_MODE       LEDC_LOW_SPEED_MODE
+#define LCD_LEDC_CHANNEL    LEDC_CHANNEL_0
+#define LCD_LEDC_DUTY_RES   LEDC_TIMER_10_BIT   // 10bit (0〜1023)
+#define LCD_LEDC_FREQUENCY  500                 // AtomS3は 500Hz 推奨 
+#define LCD_BL_GPIO         16  
+
+#define MOTOR_PIN 1
+#define PWM_FREQ_HZ     1000                // 1kHz
+#define PWM_DUTY_RES    LEDC_TIMER_10_BIT   // 10bit (0〜1023)
+#define PWM_TIMER       LEDC_TIMER_1
+#define PWM_MODE        LEDC_LOW_SPEED_MODE
+#define PWM_CHANNEL     LEDC_CHANNEL_1
+
 void app_main(void)
 {
 
